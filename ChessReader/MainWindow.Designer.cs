a2a2d444617b0b9ef5360b1e4214e356391287a1
vladstheawesome <vmabhena@gmail.com>
2023-04-09 19:25:26 +0100

    Initial - Draw Chessboard and Pieces

---
+++ ChessReader/MainWindow.Designer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ChessReader
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(24, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(592, 566);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1054, 626);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "MainWindow";
+            this.Text = "MainWindow";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ChessReader/MainWindow.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ChessReader
+{
+    public partial class MainWindow : Form
+    {
+        private readonly Board _board;
+        private readonly int _squareSize = 150;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _board = new Board();
+            LoadChessPieces();
+            DrawBoard();
+        }
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Get the position of the mouse pointer relative to the PictureBox control
+            int x = e.X / _squareSize;
+            int y = e.Y / _squareSize;
+
+            // TODO: Perform some action based on the mouse position
+        }
+
+        private void LoadChessPieces()
+        {
+            foreach (var piece in _board.Pieces)
+            {                
+                string colorName = piece.Color.ToString().ToLower();
+                string typeName = piece.Type.ToString().ToLower();
+                string pieceImagePath = $"images/pieces/{colorName}_{typeName}.png";
+                try
+                {
+                    using (var fs = new FileStream(pieceImagePath, FileMode.Open, FileAccess.Read))
+                    {
+                        piece.Image = Image.FromStream(fs);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error loading image '{pieceImagePath}': {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                // Debug code to check the color of each piece
+                Console.WriteLine($"{piece.Color} {piece.Type} {piece.Image != null}");
+            }
+        }
+
+        private void DrawBoard()
+        {
+            // Load the board background image
+            var boardImage = Image.FromFile("images/board/150.png");
+
+            // Create a new bitmap with the same size as the board image
+            var boardBitmap = new Bitmap(boardImage.Width, boardImage.Height);
+
+            // Draw the board image onto the bitmap
+            var g = Graphics.FromImage(boardBitmap);
+            g.DrawImage(boardImage, 0, 0);
+
+            // Draw the black pieces onto the bitmap
+            foreach (var piece in _board.Pieces.Where(p => p.Color == Color.Black).Reverse())
+            {
+                //var pieceImage = Image.FromFile($"images/pieces/{piece.Color}{piece.GetType().Name}.png");
+                string colorName = piece.Color.ToString().ToLower();
+                string typeName = piece.Type.ToString().ToLower();
+                string pieceImagePath = $"images/pieces/{colorName}_{typeName}.png";
+                var pieceImage = Image.FromFile(pieceImagePath);
+                g.DrawImage(pieceImage, piece.Column * _squareSize, piece.Row * _squareSize);
+            }
+
+            // Draw the white pieces onto the bitmap
+            foreach (var piece in _board.Pieces.Where(p => p.Color == Color.White).Reverse())
+            {
+                //var pieceImage = Image.FromFile($"images/pieces/{piece.Color}{piece.GetType().Name}.png");
+                string colorName = piece.Color.ToString().ToLower();
+                string typeName = piece.Type.ToString().ToLower();
+                string pieceImagePath = $"images/pieces/{colorName}_{typeName}.png";
+                var pieceImage = Image.FromFile(pieceImagePath);
+                g.DrawImage(pieceImage, piece.Column * _squareSize, piece.Row * _squareSize);
+            }
+
+            // Set the bitmap as the image for the PictureBox control
+            pictureBox1.Image = boardBitmap;
+        }
+    }
+
+    public class Board
+    {
+        public List<Piece> Pieces { get; set; }
+
+        public Board()
+        {
+            Pieces = new List<Piece>();
+
+            // Add white pieces
+            Pieces.Add(new Piece(Color.White, PieceType.Rook, 7, 0));
+            Pieces.Add(new Piece(Color.White, PieceType.Knight, 7, 1));
+            Pieces.Add(new Piece(Color.White, PieceType.Bishop, 7, 2));
+            Pieces.Add(new Piece(Color.White, PieceType.Queen, 7, 3));
+            Pieces.Add(new Piece(Color.White, PieceType.King, 7, 4));
+            Pieces.Add(new Piece(Color.White, PieceType.Bishop, 7, 5));
+            Pieces.Add(new Piece(Color.White, PieceType.Knight, 7, 6));
+            Pieces.Add(new Piece(Color.White, PieceType.Rook, 7, 7));
+            for (int i = 0; i < 8; i++)
+            {
+                Pieces.Add(new Piece(Color.White, PieceType.Pawn, 6, i));
+            }
+
+            // Add black pieces
+            Pieces.Add(new Piece(Color.Black, PieceType.Rook, 0, 0));
+            Pieces.Add(new Piece(Color.Black, PieceType.Knight, 0, 1));
+            Pieces.Add(new Piece(Color.Black, PieceType.Bishop, 0, 2));
+            Pieces.Add(new Piece(Color.Black, PieceType.Queen, 0, 3));
+            Pieces.Add(new Piece(Color.Black, PieceType.King, 0, 4));
+            Pieces.Add(new Piece(Color.Black, PieceType.Bishop, 0, 5));
+            Pieces.Add(new Piece(Color.Black, PieceType.Knight, 0, 6));
+            Pieces.Add(new Piece(Color.Black, PieceType.Rook, 0, 7));
+
+            for (int i = 0; i < 8; i++)
+            {
+                Pieces.Add(new Piece(Color.Black, PieceType.Pawn, 1, i));
+            }
+        }
+    }
+
+    public class Piece
+    {
+        public Piece(Color color, PieceType type, int row, int column)
+        {
+            if (!Enum.IsDefined(typeof(Color), color))
+                throw new ArgumentException($"Invalid color value: {color}");
+
+            if (!Enum.IsDefined(typeof(PieceType), type))
+                throw new ArgumentException($"Invalid piece type value: {type}");
+
+            Color = color;
+            Type = type;
+            Row = row;
+            Column = column;
+        }
+
+        public Color Color { get; set; }
+        public PieceType Type { get; set; }
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public Image Image { get; set; }
+    }
+
+    public enum Color
+    {
+        White,
+        Black
+    }
+
+    public enum PieceType
+    {
+        King,
+        Queen,
+        Rook,
+        Bishop,
+        Knight,
+        Pawn
+    }
+
+}
